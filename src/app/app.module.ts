71e4ba456118ef4025e40b5145ba9d9744fc3fd3
Igor Rocha <igortice@gmail.com>
2017-09-23 20:12:29 -0300

    init layout with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +8,12 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports:      [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
